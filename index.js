83996cf7b92cc6806f921af6535e92d2ea539b56
Sercan Şimşek <sercansimsek44@gmail.com>
2026-03-12 15:22:43 +0100

    server.js created

--- index.js
+++ index.js
@@ -1,3 +1,1 @@
-const aiURL = process.env.AI_URL;
 
-console.log(aiURL);
---
+++ server.js
@@ -0,0 +1,8 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const aiURL = process.env.AI_URL;
+
+console.log(aiURL);
+
