0419d65ce4c7c045df6a587806edf5c216034b64
Sercan Şimşek <sercansimsek44@gmail.com>
2026-03-13 13:41:12 +0100

    cleanup

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
+import DOMPurify from "dompurify";
 import { marked } from "marked";
-import DOMPurify from 'dompurify'
 
 const textAreaInput = document.querySelector(".form-textarea");
 
 async function handleTranslationRequest(e) {
   e.preventDefault();
 
-  const userPrompt = textAreaInput.ariaValueMax.trim();
+  const userPrompt = textAreaInput.value.trim();
   if (!userPrompt) {
     return;
   }
-
   try {
+    const response = await fetch("/api/translate", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ userPrompt }),
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error(data.message);
+    }
+
+    const translation = data.translation;
+
+    const html = marked.parse(translation);
+
+    const safeHTML = DOMPurify.sanitize(html);
+
+    document.querySelector(".result").innerHTML = safeHTML;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+document
+  .querySelector(".form-btn")
+  .addEventListener("submit", handleTranslationRequest);
+
+/* 
+ try {
     const response = await fetch("/api/translate", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
@@ -34,6 +63,4 @@ async function handleTranslationRequest(e) {
   } catch (error) {
     console.error(error)
   }
-}
-
-document.querySelector('.form-btn').addEventListener('submit', handleTranslationRequest)
+*/
