8f789a4e3a2cc09522e0bf61115dad4e34e81ba9
Sercan Şimşek <sercansimsek44@gmail.com>
2026-03-13 13:09:37 +0100

    server side logic implemented

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
+import { marked } from "marked";
+import DOMPurify from 'dompurify'
 
+const textAreaInput = document.querySelector(".form-textarea");
+
+async function handleTranslationRequest(e) {
+  e.preventDefault();
+
+  const userPrompt = textAreaInput.ariaValueMax.trim();
+  if (!userPrompt) {
+    return;
+  }
+
+  try {
+    const response = await fetch("/api/translate", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ userPrompt }),
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+        throw new Error(data.message)
+    }
+
+    const translation = data.translation
+
+    const html = marked.parse(translation)
+
+    const safeHTML = DOMPurify.sanitize(html)
+
+    document.querySelector('.result').innerHTML = safeHTML
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+document.querySelector('.form-btn').addEventListener('submit', handleTranslationRequest)
--- server.js
+++ server.js
@@ -1,8 +1,46 @@
-import dotenv from "dotenv";
+import express from "express";
+import OpenAI from "openai";
 
-dotenv.config();
+const app = express();
+app.use(express.json());
 
-const aiURL = process.env.AI_URL;
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+  baseURL: process.env.AI_URL,
+});
 
-console.log(aiURL);
+const messages = [
+  {
+    role: "system",
+    content: "You are a translator, translate the given sentence to turkish.",
+  },
+];
 
+app.post("/api/translate", async (req, res) => {
+  const { userPrompt } = req.body;
+
+  messages.push({
+    role: "user",
+    content: userPrompt,
+  });
+
+  try {
+    const response = await openai.chat.completions.create({
+      model: process.env.AI_MODEL,
+      messages,
+    });
+
+    const translation = response.choices[0].message.content;
+    response.json({ translation });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "Its not you, its us.Something went wrong on the server",
+    });
+  }
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
